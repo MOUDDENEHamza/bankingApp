b0c865c685cf2fd56a5de162d5748cc3109377f4
kalikane <soujilo54leymane@gmail.com>
2019-03-10 19:29:52 +0100

    first commit

---
+++ include/administrateur.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct _coordonnees* coordonnees;
+typedef struct _donnees_perso* donnees_perso;
+typedef struct _Compte* Compte;
+typedef struct _Titulaire_Compte* Titulaire_Comptes;
+void creer_compte(Titulaire_Comptes t);
+void modifier_compte(Titulaire_Comptes t);
+void supprimer_compte(Titulaire_Comptes t);
+void afficher_list_comptes(Titulaire_Comptes t);
+void ajouter_client();
+void modifier_infos_client(Titulaire_Comptes t);
+void afficher_list_titulaires(Compte c);
+void changer_mot(Compte c);
---
+++ src/administrateur.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/administrateur.h"
+
+typedef struct _coordonnees
+{
+    char* mail;
+    char* telnumber;
+    char* adresses;
+}_coordonnees;
+
+typedef struct _donnees_perso
+{
+    char* nom;
+    char* prenom;
+    char date_de_naissance[10];
+    _coordonnees* coordonnees;
+}_donnees_perso;
+
+
+typedef struct _Compte
+{
+    char* type;
+    char* intitule;
+    _donnees_perso* titulaires;
+}_Compte;
+
+typedef struct _Titulaire_Comptes
+{
+    _donnees_perso* donnees;
+    _Compte* list_comptes;
+}_Titulaire_Comptes;
+
+void creer_compte(Titulaire_Comptes t){
+    
+}
+
+void modifier_compte(Titulaire_Comptes t){
+
+}
+
+void supprimer_compte(Titulaire_Comptes t){
+    free(t);
+}
+
+void afficher_list_comptes(Titulaire_Comptes t){
+
+}
+
+void ajouter_client(){
+
+}
+
+void modifier_infos_client(Titulaire_Comptes t){
+
+}
+
+void afficher_list_titulaires(Compte c){
+
+}
+
+void changer_mot(Compte c){
+    
+}
+
