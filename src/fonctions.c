9b194074b57f35d08151016f6bbfc78d4d9ee815
kalikane <soujilo54leymane@gmail.com>
2019-03-13 21:47:20 +0100

    "fonction.c cree"

---
+++ src/fonctions.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/administrateur.h"
+
+void creer_compte(Titulaire_Comptes t){
+    
+}
+
+void modifier_compte(Titulaire_Comptes t){
+
+}
+
+void supprimer_compte(Titulaire_Comptes t){
+    free(t);
+}
+
+void afficher_list_comptes(Titulaire_Comptes t){
+
+}
+
+void ajouter_client(){
+
+}
+
+void modifier_infos_client(Titulaire_Comptes t){
+
+}
+
+void afficher_list_titulaires(Compte c){
+
+}
+
+void changer_mot(Compte c){
+    
+}
--- src/structures.c
+++ src/structures.c
@@ -162,37 +162,3 @@ Info_perso get_info(Titulaire_Comptes t){
 Compte get_list_comptes(Titulaire_Comptes t){
     return t->list_comptes;
 }
-
-
-
-void creer_compte(Titulaire_Comptes t){
-    
-}
-
-void modifier_compte(Titulaire_Comptes t){
-
-}
-
-void supprimer_compte(Titulaire_Comptes t){
-    free(t);
-}
-
-void afficher_list_comptes(Titulaire_Comptes t){
-
-}
-
-void ajouter_client(){
-
-}
-
-void modifier_infos_client(Titulaire_Comptes t){
-
-}
-
-void afficher_list_titulaires(Compte c){
-
-}
-
-void changer_mot(Compte c){
-    
-}
