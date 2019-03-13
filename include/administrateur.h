daabd4205e210f909c0d80930e75526d99de9526
kalikane <soujilo54leymane@gmail.com>
2019-03-13 14:57:02 +0100

    mes structures faites

--- include/administrateur.h
+++ include/administrateur.h
@@ -1,11 +1,47 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct _coordonnees* coordonnees;
-typedef struct _donnees_perso* donnees_perso;
-typedef struct _identifient* identifient;
+typedef struct _coordonnees* Coordonnees;
+void set_mail(Coordonnees c, char* m);
+void set_telnumber(Coordonnees c, char* t);
+void set_adresses(Coordonnees c, char* a);
+char* get_mail(Coordonnees c);
+char* get_telnumber(Coordonnees c);
+char* get_adresses(Coordonnees c);
+
+typedef struct _info_perso* Info_perso;
+void set_nom(Info_perso I, char* n);
+void set_prenom(Info_perso I, char* p);
+void set_date_de_naissance(Info_perso I, char* da);
+void set_coordonnees(Info_perso I, Coordonnees co);
+char* get_nom(Info_perso d);
+char* get_prenom(Info_perso I);
+char* get_date_de_naissance(Info_perso I);
+Coordonnees get_coordonnees(Info_perso I);
+
 typedef struct _Compte* Compte;
-typedef struct _Titulaire_Compte* Titulaire_Comptes;
+void set_type(Compte c, char* t);
+void set_intitule(Compte c, char* i);
+void set_solde(Compte c, float *s);
+char* get_type(Compte c);
+char* get_intitule(Compte c);
+float get_solde(Compte c);
+
+typedef struct _Titulaire_Comptes* Titulaire_Comptes;
+void set_identifient(Titulaire_Comptes t, int *i);
+void set_passeword(Titulaire_Comptes t, int *p);
+void set_info(Titulaire_Comptes t, Info_perso i);
+void set_list_comptes(Titulaire_Comptes t, Compte c);
+int get_identifient(Titulaire_Comptes t);
+int get_passeword(Titulaire_Comptes t);
+Info_perso get_info(Titulaire_Comptes t);
+Compte get_list_comptes(Titulaire_Comptes t);
+
+
+void set_mail(Coordonnees c, char* m);
+void set_telnumber(Coordonnees c, char* t);
+void set_adresses(Coordonnees c, char* a);
+
 void creer_compte(Titulaire_Comptes t);
 void modifier_compte(Titulaire_Comptes t);
 void supprimer_compte(Titulaire_Comptes t);
--- src/administrateur.c
+++ src/administrateur.c
@@ -9,36 +9,146 @@ typedef struct _coordonnees
     char* adresses;
 }_coordonnees;
 
-typedef struct _donnees_perso
+void set_mail(Coordonnees c, char* m){
+    c->mail=m;
+}
+
+void set_telnumber(Coordonnees c, char* t){
+    c->telnumber=t;
+}
+
+void set_adresses(Coordonnees c, char* a){
+    c->adresses=a;
+}
+
+char* get_mail(Coordonnees c){
+    return c->mail;
+}
+
+char* get_telnumber(Coordonnees c){
+    return c->telnumber;
+}
+
+char* get_adresses(Coordonnees c){
+    return c->adresses;
+}
+
+
+typedef struct _info_perso
 {
     char* nom;
     char* prenom;
-    char date_de_naissance[10];
-    _coordonnees* coordonnees;
-}_donnees_perso;
+    char* date_de_naissance;
+    Coordonnees coordonnees;
+}_info_perso;
 
-typedef struct _identifient
-{
-    int id;
-}_identifient;
+void set_nom(Info_perso I, char* n){
+    I->nom=n;
+}
+
+void set_prenom(Info_perso I, char* p){
+    I->prenom=p;
+}
+
+void set_date_de_naissance(Info_perso I, char* da){
+    I->date_de_naissance=da;
+}
+
+void set_coordonnees(Info_perso I, Coordonnees co){
+    I->coordonnees=co;
+}
+
+char* get_nom(Info_perso d){
+    return d->nom;
+}
+
+char* get_prenom(Info_perso I){
+    return I->prenom;
+}
+
+char* get_date_de_naissance(Info_perso I){
+    return I->date_de_naissance;
+}
+
+Coordonnees get_coordonnees(Info_perso I){
+    return I->coordonnees;
+}
 
 
 typedef struct _Compte
 {
-    identifient id;
     char* type;
     char* intitule;
-    _donnees_perso* titulaires;
+    float solde;
 }_Compte;
+
+void set_type(Compte c, char* t){
+    c->type=t;
+}
+
+void set_intitule(Compte c, char* i){
+    c->intitule=i;
+}
+
+void set_solde(Compte c, float *s){
+    c->solde=*s;
+}
+
+char* get_type(Compte c){
+    return c->type;
+}
+
+char* get_intitule(Compte c){
+    return c->intitule;
+}
+
+float get_solde(Compte c){
+    return c->solde;
+}
+
 
 typedef struct _Titulaire_Comptes
 {
-    _donnees_perso* donnees;
-    _Compte* list_comptes;
+    int identifient;
+    int passeword;
+    Info_perso info;
+    Compte list_comptes;
 }_Titulaire_Comptes;
 
+void set_identifient(Titulaire_Comptes t, int *i){
+    t->identifient=*i;
+}
+
+void set_passeword(Titulaire_Comptes t, int *p){
+    t->passeword=*p;
+}
+
+void set_info(Titulaire_Comptes t, Info_perso i){
+    t->info=i;
+}
+
+void set_list_comptes(Titulaire_Comptes t, Compte c){
+    t->list_comptes=c;
+}
+
+int get_identifient(Titulaire_Comptes t){
+    return t->identifient;
+}
+
+int get_passeword(Titulaire_Comptes t){
+    return t->passeword;
+}
+
+Info_perso get_info(Titulaire_Comptes t){
+    return t->info;
+}
+
+Compte get_list_comptes(Titulaire_Comptes t){
+    return t->list_comptes;
+}
+
 void creer_compte(Titulaire_Comptes t){
-    
+
 }
 
 void modifier_compte(Titulaire_Comptes t){
