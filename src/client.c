6a9f96b2360fc08e0060711d5b82dda15e0861e2
hamza <hz.mouddene@gmail.com>
2019-04-02 17:59:27 +0200

    TRANSFER MONEY DONE

--- src/client.c
+++ src/client.c
@@ -52,6 +52,7 @@ void transaction_list(void) {
  */
 void transfer_money(Client client, Json_object json_clients) {
     int i, j;
+    int done = 0;
     struct json_object *json_client, *json_id, *json_account_list, *json_account, *json_type, *json_entitled, *json_balance;
     char *id = (char *) malloc(SIZE), *type = (char *) malloc(SIZE), *entitled = (char *) malloc(SIZE);
     char *str_type = (char *) malloc(SIZE), *str_entitled = (char *) malloc(SIZE);
@@ -68,6 +69,11 @@ void transfer_money(Client client, Json_object json_clients) {
     printf("\nPlease, enter the amount of your transaction : ");
     scanf("%f", &amount_transfer);
 
+    if (strcmp(get_id(client), id) == 0) {
+        printf("\nSorry you can't transfer money to yourself");
+        return;
+    }
+
     if (amount_transfer <= get_balance(get_account(client)) && amount_transfer > 0) {
         n_clients = json_object_array_length(json_clients);
 
@@ -75,26 +81,45 @@ void transfer_money(Client client, Json_object json_clients) {
 
             json_client = json_object_array_get_idx(json_clients, i);
             json_object_object_get_ex(json_client, "ID", &json_id);
+            json_object_object_get_ex(json_client, "ACCOUNT LIST", &json_account_list);
+            n_accounts = json_object_array_length(json_account_list);
 
-            if (strcmp(id, json_object_get_string(json_id)) == 0 && strcmp(get_id(client), id) != 0) {
-                new_balance = get_balance(get_account(client)) - amount_transfer;
-                set_balance(get_account(client), &new_balance);
-                json_object_object_get_ex(json_client, "ACCOUNT LIST", &json_account_list);
-                n_accounts = json_object_array_length(json_account_list);
+            for (j = 0; j < n_accounts; j++) {
+                json_account = json_object_array_get_idx(json_account_list, j);
+                json_object_object_get_ex(json_account, "TYPE", &json_type);
+                json_object_object_get_ex(json_account, "ENTITLED", &json_entitled);
 
-                for (j = 0; j < n_accounts; j++) {
-                    json_account = json_object_array_get_idx(json_account_list, j);
-                    json_object_object_get_ex(json_account, "TYPE", &json_type);
-                    json_object_object_get_ex(json_account, "ENTITLED", &json_entitled);
-
-                    if (strcmp(type, json_object_get_string(json_type)) == 0 &&
-                        strcmp(entitled, json_object_get_string(json_entitled)) == 0) {
-                        json_object_object_get_ex(json_account, "BALANCE", &json_balance);
-                        /*recipient_balance = json_object_get_double(json_balance) + amount_transfer;
-                        set_balance(get_account(client), &recipient_balance);*/
-                        printf("\nTransfer done with success\n");
-                        return;
+                if (strcmp(id, json_object_get_string(json_id)) == 0 &&
+                    strcmp(type, json_object_get_string(json_type)) == 0 &&
+                    strcmp(entitled, json_object_get_string(json_entitled)) == 0) {
+                    json_object_object_get_ex(json_account, "BALANCE", &json_balance);
+                    json_object_object_foreach(json_account, key, val)
+                    {
+                        if (strcmp(key, "BALANCE") == 0) {
+                            recipient_balance = json_object_get_double(json_balance) + amount_transfer;
+                            json_object_object_add(json_account, key, json_object_new_double(recipient_balance));
+                            done++;
+                        }
                     }
+                }
+
+                if (strcmp(get_id(client), json_object_get_string(json_id)) == 0 &&
+                    strcmp("CURRENT", json_object_get_string(json_type)) == 0) {
+                    json_object_object_get_ex(json_account, "BALANCE", &json_balance);
+                    json_object_object_foreach(json_account, key, val)
+                    {
+                        if (strcmp(key, "BALANCE") == 0) {
+                            new_balance = get_balance(get_account(client)) - amount_transfer;
+                            set_balance(get_account(client), &new_balance);
+                            json_object_object_add(json_account, key, json_object_new_double(new_balance));
+                            done++;
+                        }
+                    }
+                }
+
+                if (done == 2) {
+                    printf("\nTransfer done with success\n");
+                    return;
                 }
             }
         }
@@ -102,7 +127,6 @@ void transfer_money(Client client, Json_object json_clients) {
         return;
     }
     printf("\nyour balance is insufficient to perform this operation\n");
-
 }
 
 /**
@@ -118,7 +142,6 @@ void pay_by_card(Client client, Json_object json_clients) {
     printf("\nEnter the price of the product : ");
     scanf("%f", &product_price);
 
-    printf("%s\n", json_object_get_string(json_clients));
     if (product_price <= get_balance(get_account(client))) {
         n_clients = json_object_array_length(json_clients);
 
@@ -132,13 +155,17 @@ void pay_by_card(Client client, Json_object json_clients) {
 
                 for (j = 0; j < n_accounts; j++) {
                     json_account = json_object_array_get_idx(json_account_list, j);
-                    json_object_object_get_ex(json_account, "BALANCE", &json_balance);
-                    json_object_object_foreach(json_account, key, val)
-                    {
+                    json_object_object_get_ex(json_account, "TYPE", &json_type);
+                    printf("\n%s\n", json_object_get_string(json_type));
+                    if (strcmp(json_object_get_string(json_type), "CURRENT") == 0) {
+                        json_object_object_get_ex(json_account, "BALANCE", &json_balance);
+                        json_object_object_foreach(json_account, key, val)
+                        {
 
-                        if (strcmp(key, "BALANCE") == 0) {
-                            new_balance = json_object_get_double(json_balance) - product_price;
-                            json_object_object_add(json_account, key, json_object_new_double(new_balance));
+                            if (strcmp(key, "BALANCE") == 0) {
+                                new_balance = json_object_get_double(json_balance) - product_price;
+                                json_object_object_add(json_account, key, json_object_new_double(new_balance));
+                            }
                         }
                     }
                 }
